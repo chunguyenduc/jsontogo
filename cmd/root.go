ab2287c8b9b29865790ed4c5dfd6cae1177261fd
chunguyenduc <nguyenducchu1999@gmail.com>
2023-03-10 12:41:38 +0700

    update cobra cli

--- cmd/root.go
+++ cmd/root.go
@@ -1,51 +1,73 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2023 Duc Chu nguyenducchu1999@gmail.com
 */
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
+	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jsontogo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "jsontogo - a CLI to convert JSON to Go struct",
+	Long:  `jsontogo - a CLI to convert JSON to Go struct`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// fmt.Println(args)
+		var input []byte
+		fileInput, err := cmd.Flags().GetString("file_input")
+		if err != nil {
+			fmt.Println(err)
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		if len(fileInput) > 0 {
+			fileExtension := filepath.Ext(fileInput)
+			if fileExtension != ".json" {
+				fmt.Printf("Error: not JSON file")
+			} else {
+				jsonFile, err := os.Open(fileInput)
+
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				defer jsonFile.Close()
+
+				input, _ = ioutil.ReadAll(jsonFile)
+			}
+		} else {
+			input = []byte(args[1])
+		}
+		fmt.Println(input)
+		result, err := jsonToGo(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		print(result)
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		glog.Errorln(err)
 		os.Exit(1)
 	}
 }
 
+// var Verbose bool
+var FileInput string
+
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jsontogo.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	rootCmd.PersistentFlags().StringP("author", "a", "Duc Chu", "author name for copyright attribution")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().StringP("input", "", "", "Input JSON string")
+	rootCmd.Flags().StringP("file_input", "f", "", "Read input from JSON file")
+	rootCmd.Flags().StringP("file_output", "o", "", "Write output to Go file")
+
 }
-
-
