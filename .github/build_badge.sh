20764bffd87b7ee5eafd3e33679d892bc58c44fc
chunguyenduc <nguyenducchu1999@gmail.com>
2023-03-10 18:31:22 +0700

    add Go coverage badge

---
+++ .github/build_badge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COVERAGE=`go tool cover -func=coverage.out | grep total: | grep -Eo '[0-9]+\.[0-9]+'`
+echo $COVERAGE
+COLOR=orange
+if (( $(echo "$COVERAGE <= 50" | bc -l) )) ; then
+    COLOR=red
+    elif (( $(echo "$COVERAGE > 80" | bc -l) )); then
+    COLOR=green
+fi
+curl "https://img.shields.io/badge/coverage-$COVERAGE%25-$COLOR" > badge.svg
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # jsontogo
 
 [![Deployment](https://github.com/chunguyenduc/jsontogo/actions/workflows/ci.yml/badge.svg?event=push)](https://github.com/chunguyenduc/jsontogo/actions/workflows/ci.yml)
+![Coverage](https://github.com/chunguyenduc/jsontogo/blob/badge/badge.svg?branch=badge)
 
 > A CLI to convert JSON to Go struct
